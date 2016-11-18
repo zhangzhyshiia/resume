52accaa00cc11f305d55b944c2144bca407ecbf7
zhangzhyshiia <982482827@qq.com>
2016-11-18 16:12:08 +0800

    's'

--- js/index.js
+++ js/index.js
@@ -1,14 +1,14 @@
 /**
  * Created by Administrator on 2016/11/8.
  */
-(function () {
-    var desW = 640,
-        winW = document.documentElement.clientWidth,
-        oMain=document.querySelector('.main');
+(function(desW){
+    var winW=document.documentElement.clientWidth,
+        oMain=document.getElementById('main'),
+        n=winW/desW;
     if(winW>desW){
         oMain.style.margin='0 auto';
         oMain.style.width=desW+'px';
         return;
     }
-    document.documentElement.style.fontSize = winW/desW*100+"px";
-})()
+    document.documentElement.style.fontSize=n*100+'px';
+})(640);
